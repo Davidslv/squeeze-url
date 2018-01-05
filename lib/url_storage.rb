c362b20f0aed9e7b26ce143deb763028cb9b0122
David Silva <david.silva@digital.cabinet-office.gov.uk>
2018-01-05 14:12:05 +0000

    Format and validate given url

--- lib/url_storage.rb
+++ lib/url_storage.rb
@@ -1,4 +1,7 @@
+require 'uri'
 require 'securerandom'
+
+class RequestError < StandardError; end
 
 class UrlStorage
   def initialize
@@ -7,6 +10,9 @@ class UrlStorage
   end
 
   def save(url)
+    url = format_url(url)
+    validate(url)
+
     return store[url] if store[url]
     short_url = SecureRandom.urlsafe_base64[0..5]
     store_reversed[short_url] ||= url
@@ -19,4 +25,22 @@ class UrlStorage
 
   private
     attr_reader :store, :store_reversed
+
+  def format_url(url)
+    uri = URI.parse(url)
+
+    if uri.scheme.nil? && uri.host.nil? && !uri.path.nil?
+      uri.scheme = "http"
+      uri.host = uri.path
+      uri.path = ""
+    end
+
+    uri.to_s
+  end
+
+  def validate(url)
+    if (url =~ URI.regexp).nil?
+      raise RequestError, "Invalid URL has been provided: #{url}"
+    end
+  end
 end
--- spec/url_storage_spec.rb
+++ spec/url_storage_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe UrlStorage do
   let(:url) { "http://helloworld.com" }
 
   describe '#save' do
-    context 'when a url is given' do
+    context 'when a valid url is given' do
       it 'stores a url and returns the short_url string value' do
         expect(
           url_storage.save(url)
@@ -19,6 +19,16 @@ RSpec.describe UrlStorage do
         expect(
           url_storage.save(url)
         ).to eql(first_short_url)
+      end
+    end
+
+    context 'when a url is given without a schema' do
+      it 'stores the url with the "http" schema' do
+        short_url = url_storage.save("helloworld.com")
+
+        expect(
+          url_storage.read(short_url)
+        ).to eql("http://helloworld.com")
       end
     end
   end
